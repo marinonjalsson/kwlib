fca2ab957861f6b5ff33c3e3c1d9d9222692d90f
Marinó á skrifbord <marino.njalsson@gmail.com>
2023-12-01 12:38:26 +0000

    ...

--- lib/flow.dart
+++ lib/flow.dart
@@ -2,4 +2,5 @@ export 'src/flow/flow_builder.dart';
 export 'src/flow/flow_ctl.dart';
 export 'src/flow/flow_pipe.dart';
 export 'src/flow/flow_ram.dart';
+export 'src/flow/flow_update.dart';
 export 'src/flow/ram.dart';
