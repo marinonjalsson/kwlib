259c4df121b87ccf0fe8e67112cd0e6fb5edb7df
Marinó á skrifbord <marino.njalsson@gmail.com>
2023-12-01 12:36:55 +0000

    Updated FlowRam to use FlowUpdate.

--- lib/src/flow/ram.dart
+++ lib/src/flow/ram.dart
@@ -1,7 +1,7 @@
-import 'package:kwlib/src/flow/flow_ram.dart';
+import 'package:kwlib/src/flow/flow_update.dart';
 
 /// Can be use as parent class for *Ram class mixted with *Bloc.
 abstract class Ram {
   /// Can be used to update all the page.
-  final ui = FlowRam<void>(null);
+  final ui = FlowUpdate();
 }
