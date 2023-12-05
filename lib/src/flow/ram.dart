17405c74c73db3c2c91f80d092834df98457fc8a
Marinó á skrifbord <marino.njalsson@gmail.com>
2023-12-05 20:14:25 +0000

    Tók út ui = FlowUpdate úr Ram. Of almennt.

--- lib/src/flow/ram.dart
+++ lib/src/flow/ram.dart
@@ -1,7 +1,7 @@
-import 'package:kwlib/src/flow/flow_update.dart';
+//import 'package:kwlib/src/flow/flow_update.dart';
 
 /// Can be use as parent class for *Ram class mixted with *Bloc.
 abstract class Ram {
-  /// Can be used to update all the page.
-  final ui = FlowUpdate();
+  // Can be used to update all the page.
+  //final ui = FlowUpdate();
 }
